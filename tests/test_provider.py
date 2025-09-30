1501deb96ee8ced434c5fef82fff694ccb1b7488
m-misiura <mmisiura@redhat.com>
2025-09-30 17:01:39 +0100

    :construction: added a new way of loading the provider using the RemoteProviderSpec while mainting backwards compatibility

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -1,4 +1,13 @@
-from llama_stack.providers.datatypes import AdapterSpec, Api, ProviderSpec
+from llama_stack.providers.datatypes import Api, ProviderSpec
+
+try:
+    from llama_stack.providers.datatypes import AdapterSpec
+
+    USE_LEGACY = True
+except ImportError:
+    from llama_stack.providers.datatypes import RemoteProviderSpec
+
+    USE_LEGACY = False
 
 from llama_stack_provider_trustyai_fms.provider import (
     get_provider_spec,
@@ -9,26 +18,46 @@ from llama_stack_provider_trustyai_fms.provider import (
 class TestProviderFunctions:
     def test_get_provider_spec(self):
         spec = get_provider_spec()
-
         assert isinstance(spec, ProviderSpec)
         assert spec.api == Api.safety
-        assert isinstance(spec.adapter, AdapterSpec)
-        assert spec.adapter.adapter_type == "trustyai_fms"
-        assert (
-            spec.adapter.config_class
-            == "llama_stack_provider_trustyai_fms.config.FMSSafetyProviderConfig"
-        )
-        assert spec.adapter.module == "llama_stack_provider_trustyai_fms"
+
+        if USE_LEGACY:
+            assert isinstance(spec.adapter, AdapterSpec)
+            assert spec.adapter.adapter_type == "trustyai_fms"
+            assert (
+                spec.adapter.config_class
+                == "llama_stack_provider_trustyai_fms.config.FMSSafetyProviderConfig"
+            )
+            assert spec.adapter.module == "llama_stack_provider_trustyai_fms"
+        else:
+            assert isinstance(spec, RemoteProviderSpec)
+            assert spec.provider_type == "remote::trustyai_fms"
+            assert spec.adapter_type == "trustyai_fms"
+            assert (
+                spec.config_class
+                == "llama_stack_provider_trustyai_fms.config.FMSSafetyProviderConfig"
+            )
+            assert spec.module == "llama_stack_provider_trustyai_fms"
 
     def test_get_shields_provider_spec(self):
         spec = get_shields_provider_spec()
-
         assert isinstance(spec, ProviderSpec)
         assert spec.api == Api.shields
-        assert isinstance(spec.adapter, AdapterSpec)
-        assert spec.adapter.adapter_type == "trustyai_fms"
-        assert (
-            spec.adapter.config_class
-            == "llama_stack_provider_trustyai_fms.config.FMSSafetyProviderConfig"
-        )
-        assert spec.adapter.module == "llama_stack_provider_trustyai_fms"
+
+        if USE_LEGACY:
+            assert isinstance(spec.adapter, AdapterSpec)
+            assert spec.adapter.adapter_type == "trustyai_fms"
+            assert (
+                spec.adapter.config_class
+                == "llama_stack_provider_trustyai_fms.config.FMSSafetyProviderConfig"
+            )
+            assert spec.adapter.module == "llama_stack_provider_trustyai_fms"
+        else:
+            assert isinstance(spec, RemoteProviderSpec)
+            assert spec.provider_type == "remote::trustyai_fms"
+            assert spec.adapter_type == "trustyai_fms"
+            assert (
+                spec.config_class
+                == "llama_stack_provider_trustyai_fms.config.FMSSafetyProviderConfig"
+            )
+            assert spec.module == "llama_stack_provider_trustyai_fms"
